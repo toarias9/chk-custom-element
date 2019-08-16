9b6b90354cc88b91504d80392ebbea3225982ab2
Leandro Devesa <leandro.devesa@despegar.com>
2019-08-16 11:20:50 -0300

    2 wcs

---
+++ angular-element-a/webpack.extra.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+
+module.exports = {
+  "externals": {
+      "rxjs": "rxjs",
+      "@angular/core": "ng.core",
+      "@angular/common": "ng.common",
+      "@angular/platform-browser": "ng.platformBrowser",
+      "@angular/platform-browser-dynamic": "ng.platformBrowserDynamic",
+      "@angular/elements": "ng.elements",
+      "@angular/compiler": "ng.compiler"
+  }
+}
